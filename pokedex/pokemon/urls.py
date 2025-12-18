b10437cd6a728c281baa2dbf09fd3326c1120f2c
Zelfaesque <fiona.fauv@gmail.com>
2025-12-18 16:28:56 +0100

    Update : New URL Index - Style Base.html

--- pokedex/pokemon/urls.py
+++ pokedex/pokemon/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'pokemon'
 
 urlpatterns = [
-    path('', views.pokedex_list, name='list'),
+    path('', views.index, name='index'),
+    path('list/', views.pokedex_list, name='list'),
     path('pokemon/<str:pokemon_id>/', views.pokemon_detail, name='detail'),
 ]
